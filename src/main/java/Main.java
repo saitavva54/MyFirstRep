732db5c43c5f1bd05f5c4c8b4d8f5bceb25feb76
sgoutham <sai.tavva54@gmail.com>
2025-12-04 18:19:50 -0800

    First commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.Collections;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public void temp() {
+        System.out.println("Hello and welcome!");
+    }
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+        System.out.println(CollectionUtils.isEmpty(Collections.emptyList()));
+    }
+}
